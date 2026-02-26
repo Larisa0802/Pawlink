0e438d7e3c4fa6636075cba323e127b27580fa60
iNakky <albarojasespejo@gmail.com>
2026-02-26 15:22:12 +0100

    Register v2

--- estructura-web/controllers/user_controller.mjs
+++ estructura-web/controllers/user_controller.mjs
@@ -1,3 +1,4 @@
+import { FIREBASE_API_KEY } from "../config/keys.mjs";
 export const showRegisterForm = (req, res) => {
   res.render("completes/register");
 };
@@ -9,12 +10,18 @@ export const submitRegister = async (req, res) => {
 
   // 1. Crear usuario en Firebase
   const firebaseResponse = await fetch(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCk2VlVB7RoNYmLCfeuHeF1ucBzfnA5YpM",
+    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_API_KEY}`,
     {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password, returnSecureToken: true })
-    }
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true,
+      }),
+    },
   );
 
   const firebaseData = await firebaseResponse.json();
@@ -29,7 +36,7 @@ export const submitRegister = async (req, res) => {
   const response = await fetch("http://localhost:3001/api/users/register", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ id: uid, nombre, email })
+    body: JSON.stringify({ id: uid, nombre, email }),
   });
 
   const data = await response.json();
