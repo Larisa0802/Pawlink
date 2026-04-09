85550acbb2169d63e33832d2890db0a869a95ef8
Larisa <larisa.mancebo@ejemplo.com>
2026-04-09 22:18:51 +0200

    avance front

--- estructura-web/routes/animal_routes.mjs
+++ estructura-web/routes/animal_routes.mjs
@@ -5,5 +5,8 @@ const router = Router();
 
 router.get("/animales", animalController.getAllAnimales);
 router.get("/animales/:id", animalController.getAnimalById);
+router.get("/adopciones", (req, res) =>{
+    res.render("completes/vistaEleccion")
+})
 
 export default router;
