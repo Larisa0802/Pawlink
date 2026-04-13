3609054a6f9db0244c9a1433d9d1ec7959d59fa3
Larisa <larisa.mancebo@ejemplo.com>
2026-04-13 12:14:01 +0200

    match empezado

--- estructura-web/routes/animal_routes.mjs
+++ estructura-web/routes/animal_routes.mjs
@@ -5,8 +5,17 @@ const router = Router();
 
 router.get("/animales", animalController.getAllAnimales);
 router.get("/animales/:id", animalController.getAnimalById);
-router.get("/adopciones", (req, res) =>{
-    res.render("completes/vistaEleccion")
-})
+router.get("/vistaEleccion", (req, res) => {
+    const userData = req.cookies["datosUsuario"] || null;
+
+    if (!userData) {
+        return res.redirect("/login");
+    }
+
+    res.render("completes/vistaEleccion", { userData, active: "vistaEleccion" });
+});
+
+router.get("/adopciones", animalController.getCatalogo);
+router.post("/encuesta", animalController.enviarEncuesta);
 
 export default router;
