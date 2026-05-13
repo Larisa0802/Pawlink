dfc91e342496fe3c80c15e3f13482e0dfe51f02d
iNakky <alba.rojas0305@gmail.com>
2026-05-13 22:14:26 +0200

    Borrar todo el contenido de la rama

--- estructura-web/config/keys.mjs
+++
@@ -1,1 +0,0 @@
-export const FIREBASE_API_KEY = "AIzaSyCCQLusEQFOlkEp1IGy_bxDt3cOtc3tAkI";
--- estructura-web/routes/procesos_routes.mjs
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import procesoController from "../controllers/proceso_controller.mjs";
-
-const router = express.Router();
-
-router.get("/procesos", procesoController.showProcesos);
-
-export default router;
